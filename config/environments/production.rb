232be1d18ecc0f2a2820b3419fdafd26831664b9
Adem Dinarević <ademdinarevic@gmail.com>
2025-12-07 14:27:42 +0100

    Configure ActionCable allowed origins for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [
+    "https://igbn-nuernberg-d2981e27b782.herokuapp.com",
+    /https:\/\/igbn-nuernberg-d2981e27b782\.herokuapp\.com/
+  ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
