8c77e230f6a5438c565cc8ca64e259fda7fd4822
Adem Dinarević <ademdinarevic@gmail.com>
2026-01-23 10:11:01 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "speaker#index"
+  root "listener#index"
 
   # Admin backoffice
   namespace :admin do
